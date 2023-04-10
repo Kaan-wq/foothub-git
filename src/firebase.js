0dcdfe8b3275a48d844fb19321de52c9fe4752be
Adam Ezzaim <ezzaim@kth.se>
2023-04-10 07:43:00 +0000

    firebase files

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVn193nrLf6jkSWcdXB9IALmgQFqhU6j0",
+  authDomain: "foothub-69.firebaseapp.com",
+  projectId: "foothub-69",
+  storageBucket: "foothub-69.appspot.com",
+  messagingSenderId: "605006746987",
+  appId: "1:605006746987:web:f112ffed8c94707b8b282e",
+  measurementId: "G-YY2BEJGJFE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
