d947ddb9bcf85a9b997ae251b051ddd6f220f4ed
Adam Ezzaim <ezzaim@kth.se>
2023-05-16 17:46:46 +0200

    Fix: Fixing the api with force re render and promise no data + a bit of code cleaning

--- src/react/matchesPresenter.js
+++ src/react/matchesPresenter.js
@@ -1,18 +1,30 @@
 import MatchesView from "../views/matchesView";
 import {useState, useEffect} from "react";
 import promiseNoData from "../views/promiseNoData";
+import { getLiveMatches } from "../matchesSource";
+import resolvePromise from "../resolvePromise";
 
 
 function Matches({model}) {
+    const [, forceReRender ]= useState();
+    function reRenderACB(){ forceReRender(new Object()); } 
+    function updateOnPromise(promise, reRender){
+        if(!promise) return
+        promise.then(reRender).catch(reRender);   
+        reRender();  // because the promise has changed. Will display the loading image.
+    }
+    
     function lifeACB(){
-        //console.log("Mounted");
-        model.getMatches();
+        model.getMatches()
+        updateOnPromise(model.matchesPromiseState.promise, reRenderACB)
+        
         return function ripACB(){};
     }
 
     useEffect(lifeACB, []);
 
-    return <MatchesView matches={model.matchesPromiseState.data}></MatchesView>;
+
+    return promiseNoData(model.matchesPromiseState) || <MatchesView matches={model.matchesPromiseState.data}></MatchesView>;
 }
 
 export default Matches;
